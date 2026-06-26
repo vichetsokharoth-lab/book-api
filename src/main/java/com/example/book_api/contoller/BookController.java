5a293c3bf91b7e44c9f3ac6bce415a1fff9c4375
camit1 <https://github.com/vichetsokharoth-lab/camit1.git>
2026-06-26 11:16:35 +0700

    13th commit

--- src/main/java/com/example/book_api/contoller/BookController.java
+++ src/main/java/com/example/book_api/contoller/BookController.java
@@ -14,7 +14,7 @@ import com.example.book_api.model.Book;
 import com.example.book_api.service.BookService;
 
 @Controller
-public class BookController {
+public class BookController {  //test
 
     private final BookService bookService;
 
